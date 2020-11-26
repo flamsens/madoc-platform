5bd86074bdeef4b534f39b6a035f8c9613658a3a
Stephen Fraser <stephen.fraser@digirati.com>
2020-11-26 11:30:19 +0000

    Config

--- services/madoc-ts/src/frontend/shared/atoms/DemoButton.tsx
+++ services/madoc-ts/src/frontend/shared/atoms/DemoButton.tsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const DemoButton = styled.button`
   background: #93e683;
-  padding: 1em 2em;
+  padding: 1.4em 2em;
   width: 100%;
   border: none;
+  border-radius: 5px;
 `;
